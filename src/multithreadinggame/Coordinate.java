9a402efe3a04678390ec70baa3b9d8b0e9632220
MSI\User <enricojulian@gmail.com>
2020-07-05 20:46:22 +0800

    new class for to store coordinate

---
+++ src/multithreadinggame/Coordinate.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multithreadinggame;
+
+/**
+ *
+ * @author User
+ */
+public class Coordinate {
+    
+    public double x;
+    public double y;
+    
+    Coordinate(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public double getX() {
+        return x;
+    }
+    
+    public double getY() {
+        return y;
+    }
+    
+    public String getString(){
+        String str = "(" + x + ", " + y + ")";
+        return  str;
+    }
+    
+}
